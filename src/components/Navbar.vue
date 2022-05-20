a9c38dc26e84cb6c984016409692fc6bb821260c
Andrelourencogit <lourencoandre80@gmail.com>
2022-05-20 10:35:20 -0300

    Remove componentes não usados

---
+++ src/components/Navbar.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+defineProps([
+    'logo',
+    'alt'
+])
+</script>
+
+<template>
+  <div>
+    <div id="nav">
+      <RouterLink id="logo-url" to="/"><img id="logo" :src="logo" :alt="alt"></RouterLink>
+      <RouterLink to="/">Fazer Pedido</RouterLink>
+      <RouterLink to="/pedidos">Pedidos</RouterLink>
+    </div>
+  </div>
+  <RouterView />
+</template>
+
+<style scoped>
+  #nav {
+    background-color: #222;
+    border-bottom: 4px solid #111;
+    padding: 15px 50px;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+  }
+  #nav #logo-url {
+    margin: auto;
+    margin-left: 0;
+  }
+  #logo {
+    width: 40px;
+    height: 40px;
+  }
+  #nav a {
+    color: #fcba03;
+    text-decoration: none;
+    margin: 12px;
+    transition: .5s;
+  }
+  #nav a:hover {
+    color: #FFF;
+  }
+</style>
